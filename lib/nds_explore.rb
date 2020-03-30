8abb56ab920bdcafde805405a53642f17574f51b
Adao Natalino <sansonov@gmail.com>
2020-03-30 15:32:38 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -9,7 +9,29 @@ end
 
 def print_first_directors_movie_titles
   
-  
+  x = 0
+while x < nds.length do
+  puts "Linha #{x} has #{nds[x]} columns"
+ 
+  y = 0
+  while y < vm[x].length do
+    coord = "#{x}, #{y}"
+    z = nds[x][y].length
+    # Remember \t is a TAB character for indentation
+    puts "\tCoordinate [#{coord}] points to an #{vm[row_index][column_index].class} of length #{inner_len}"
+ 
+    inner_index = 0
+    while inner_index < inner_len do
+      puts "\t\t (#{coord}, #{inner_index}) is: #{vm[row_index][column_index][inner_index]}"
+      inner_index += 1
+    end
+ 
+    column_index += 1
+  end
+ 
+  row_index += 1
+end
+
   return nd
   
 end
